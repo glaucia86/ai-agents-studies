e185165de2a7163157f4563a3e9fb045af203494
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2025-06-25 21:56:50 -0300

    feat: add initial setup for Hugging Face LLM example with TypeScript
    - Created package.json with dependencies for Hugging Face API and TypeScript support.
    - Added tsconfig.json for TypeScript configuration, targeting modern Node.js features.

---
+++ 01-resource/units/unit-01/samples/hf-llm-example/index.ts
@@ -0,0 +1,49 @@
+import ModelClient, { isUnexpected } from '@azure-rest/ai-inference'; 
+import { AzureKeyCredential } from '@azure/core-auth';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+async function callLLM(prompt: string): Promise<string> {
+  const token = process.env.GROK_GITHUB_MODEL_TOKEN
+
+  if (!token) {
+    throw new Error('Token de autenticação não encontrado. Verifique a variável de ambiente GROK_GITHUB_MODEL_TOKEN.');
+  }
+
+  const endpoint = 'https://models.github.ai/inference';
+  const modelName = "xai/grok-3-mini";
+
+  const client = ModelClient(
+    endpoint,
+    new AzureKeyCredential(token)
+  );
+
+  const response = await client.path("/chat/completions").post({
+    body: {
+      messages: [
+        { role: "system", content: "Sou um assistente de IA treinado para responder perguntas."},
+        { role: "user", content: prompt }
+      ],
+      temperature: 0.7,
+      top_p: 1.0,
+      model: modelName,
+    }
+  });
+
+  if (isUnexpected(response)) {
+    throw new Error(`Erro da API: ${JSON.stringify(response.body.error)}`);
+  }
+
+  const content = response.body.choices[0].message?.content;
+
+  if(!content) {
+    throw new Error('Resposta inesperada do modelo: conteúdo vazio.');
+  }
+
+  return content;
+}
+
+callLLM("Qual é a capital da França?")
+  .then(response => console.log("Resposta...: ", response))
+  .catch(error => console.error("Erro...: ", error));
