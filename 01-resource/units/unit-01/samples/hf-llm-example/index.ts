639e8b48aa5829c7ea7ded906ff9b63dda068dd5
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2025-06-30 18:05:27 -0300

    feat: integrate OpenAI API for LLM calls and update dependencies

--- 01-resource/units/unit-01/samples/hf-llm-example/index.ts
+++ 01-resource/units/unit-01/samples/hf-llm-example/index.ts
@@ -1,49 +1,47 @@
-import ModelClient, { isUnexpected } from '@azure-rest/ai-inference'; 
-import { AzureKeyCredential } from '@azure/core-auth';
+import OpenAI from 'openai';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+const token = process.env.OPEN_API_GITHUB_MODEL_TOKEN;
+
+if (!token) {
+  throw new Error("Token de autenticação não encontrado. Certifique-se de definir a variável de ambiente OPEN_API_GITHUB_MODEL_TOKEN.");
+}
+
+const endpoint = 'https://models.github.ai/inference';
+const modelName = 'openai/gpt-4o';
+
 async function callLLM(prompt: string): Promise<string> {
-  const token = process.env.OPEN_API_GITHUB_MODEL_TOKEN
-
-  if (!token) {
-    throw new Error('Token de autenticação não encontrado. Verifique a variável de ambiente GROK_GITHUB_MODEL_TOKEN.');
-  }
-
-  const endpoint = process.env.OPEN_API_GITHUB_MODEL_ENDPOINT ||'';
-  const modelName = "openai/gpt-4o";
-
-  const client = ModelClient(
-    endpoint,
-    new AzureKeyCredential(token)
-  );
-
-  const response = await client.path("/chat/completions").post({
-    body: {
-      messages: [
-        { role: "system", content: "Sou um assistente de IA treinado para responder perguntas."},
-        { role: "user", content: prompt }
-      ],
-      temperature: 0.7,
-      top_p: 1.0,
-      model: modelName,
-    }
+  const client = new OpenAI({
+    baseURL: endpoint,
+    apiKey: token,
   });
 
-  if (isUnexpected(response)) {
-    throw new Error(`Erro da API: ${JSON.stringify(response.body.error)}`);
+  const responseLLM = await client.chat.completions.create({
+    messages: [
+      { role: 'system', content:'Você é um assistente treinaado para responder perguntas!' },
+      { role: 'user', content: prompt  }
+    ],
+    temperature: 0.7,
+    model: modelName,
+    max_completion_tokens: 500,
+    top_p: 1.0,
+  });
+
+  if (!responseLLM) {
+    throw new Error("Resposta do LLM não recebida.");
   }
 
-  const content = response.body.choices[0].message?.content;
+  const content = responseLLM.choices[0].message?.content;
 
-  if(!content) {
-    throw new Error('Resposta inesperada do modelo: conteúdo vazio.');
+  if (!content) {
+    throw new Error("Conteúdo da resposta do LLM não encontrado.");
   }
 
   return content;
 }
 
 callLLM("Qual é a capital da França?")
-  .then(response => console.log("Resposta...: ", response))
-  .catch(error => console.error("Erro...: ", error));
+  .then(response => console.log("Resposta do LLM...: ", response))
+  .catch(error => console.error("Erro ao chamar o LLM...: ", error));
